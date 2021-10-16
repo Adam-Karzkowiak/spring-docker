c53794fea99dcfedf30b257a760f711efc2af698
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-10-16 20:47:00 +0200

    Dockerfile prod+test containers

--- src/main/java/com/karzkowiak/springdocker/HelloController.java
+++ src/main/java/com/karzkowiak/springdocker/HelloController.java
@@ -1,13 +1,17 @@
 package com.karzkowiak.springdocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
 
+    @Value("${env.name}")
+    private String envName;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello container";
+        return "hello container " + envName;
     }
 }
